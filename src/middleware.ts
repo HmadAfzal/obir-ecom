28ec8ef26b904d85b27683d819fc169b343b6ce5
Hmad Afzal <hmadafzal00@gmail.com>
2024-10-14 09:29:52 +0500

    middleware fixed

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,27 +1,33 @@
-import { NextRequest, NextResponse } from 'next/server';
-import { getToken } from 'next-auth/jwt';
-export { default } from 'next-auth/middleware';
+import { NextRequest, NextResponse } from "next/server";
+import { getToken } from "next-auth/jwt";
+export { default } from "next-auth/middleware";
 
 export const config = {
-  matcher: ['/', '/login', '/signup', '/verify/:email', '/cart'  , '/checkout'],
+  matcher: ["/", "/login", "/signup", "/cart", "/checkout"],
 };
 
 export async function middleware(request: NextRequest) {
-  const token = await getToken({ req: request, secret: process.env.NEXTAUTH_SECRET });
-  const url = request.nextUrl.clone();
+  const token = await getToken({
+    req: request,
+    secret: process.env.NEXTAUTH_SECRET,
+  });
+  const url = request.nextUrl;
 
-  if (token) {
-
-    if (['/login', '/signup','/verify/:email'].includes(url.pathname)) {
-      return NextResponse.redirect(new URL('/', request.url));
-    }
-    return NextResponse.next();
+  if (
+    token &&
+    (url.pathname.startsWith("/login") || url.pathname.startsWith("/signup"))
+  ) {
+    return NextResponse.redirect(new URL("/", request.url));
   }
 
-  const restrictedPaths = ['/checkout', '/cart',];
-  if (!token && restrictedPaths.includes(url.pathname)) {
-    return NextResponse.redirect(new URL('/login', request.url));
+  if (
+    !token &&
+    (url.pathname.startsWith("/cart") || url.pathname.startsWith("/checkout"))
+  ) {
+    return NextResponse.redirect(new URL("/login", request.url));
   }
 
-  return NextResponse.next();
+  const response = NextResponse.next();
+  response.headers.set("Cache-Control", "no-store, must-revalidate");
+  return response;
 }
